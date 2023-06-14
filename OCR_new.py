2b6aa37cc58c7c2006fab11180bc1287bc92e1b3
Yoni Ifrah <73611785+YoniIfrah@users.noreply.github.com>
2023-06-14 14:33:41 +0300

    Update OCR_new.py
    bug fix

--- OCR_new.py
+++ OCR_new.py
@@ -7,7 +7,8 @@ captioner = pipeline("image-to-text",model="jinhybr/OCR-Donut-CORD")
 ERROR_MSG = "not found"
 
 def split_string(input_string):
-    pattern = r"[,./-:d]+"  # Regular expression pattern to match numbers, hyphens, forward slashes, and periods
+    # pattern = r"[,./-:d]+"  # Regular expression pattern to match numbers, hyphens, forward slashes, and periods
+    pattern = r"\b(\d{0,4}[-,.:/]\d{0,4}(?:[-,.:/]\d{0,4})?)\b"
     split_values = re.findall(pattern, input_string)
     pattern = r"\d+"  # Regular expression pattern to match one or more digits
     result = [string for string in split_values if re.search(pattern, string) and len(string) > 4]
@@ -28,10 +29,10 @@ def  run_expiration_date_workflow(string):
         string (str): string with date
 
     Returns:
-        str: the date that found from the string or not found
+        str: the date that found from the string in format dd/mm/yyyy or not found
     """
     # Define regex pattern for date format
-    print("string = ",string )
+    print("string ===== ",string )
     strings = split_string(string)
     valid_dates = []
 
@@ -41,32 +42,49 @@ def  run_expiration_date_workflow(string):
         for i in range(len(strings)):
             if len(strings[i]) > 2:
                 strings[i] = strings[i].replace(char, "/")
+    print("string ===== ",strings )
+
+    for i in range(len(strings)):
+        if len(strings[i]) > 4 and strings[i].count('/') != 2:
+            strings[i] = fix_date(strings[i][:4]) + strings[i][4:]
+
 
     # all the dates will include year
     for i in range(len(strings)):
-        if strings[i].count('/') == 2:
-            year = '20'+strings[i][-2:]
-            valid_dates.append(strings[i][:-2] + year)
-
-
-        elif strings[i].count('/') == 1:
-            strings[i] += '/' + datetime.date.today().strftime("%Y")
-            valid_dates.append(strings[i])
-
         # numbers -> date , fix date func
-        elif strings[i].count('/') == 0:
+        if ERROR_MSG in strings[i]:
+            continue
+        if strings[i].count('/') == 0:
             strings[i] = fix_date(strings[i])
             if strings[i] != ERROR_MSG:
                 strings[i] += '/' + datetime.date.today().strftime("%Y")
                 valid_dates.append(strings[i])
 
+        elif strings[i].count('/') == 1:
+            strings[i] += '/' + datetime.date.today().strftime("%Y")
+            valid_dates.append(strings[i])
+
+        elif strings[i].count('/') == 2:
+            year = '20'+strings[i][-2:]
+            valid_dates.append(strings[i][:-2] + year)
+
+
+
+
+
+    
     result=[]
     result = [valid_date for valid_date in valid_dates if is_valid_date(valid_date)]
+    print("##########")
     if result:
+        print(result[0])
+        print("##########")
         return result[0]
     else: 
+        print(ERROR_MSG)
+        print("##########")
         return ERROR_MSG
-
+    
 
 
 def fix_date(date:str):
@@ -80,29 +98,31 @@ def fix_date(date:str):
     Returns:
         str: dd/mm date format or not found
     """
-    day = ''
-    month = ''
-    
-    if date[0] == '0':
-        day = date[1]
-        if date[2] == '0':
-            month = date[3]
-        else:
-            month = date[2:4]
-    else:
-        day = date[:2]
-        if date[2] == '0':
-            month = date[3]
-        else:
-            month = date[2:4]
-
-    #   in case we got invalid date
-    if int(month) > 12 or int(day) > 31:
-        return ERROR_MSG
+    try:
+        day = ''
+        month = ''
         
-    return f"{day}/{month}"
+        if date[0] == '0':
+            day = date[1]
+            if date[2] == '0':
+                month = date[3]
+            else:
+                month = date[2:4]
+        else:
+            day = date[:2]
+            if date[2] == '0':
+                month = date[3]
+            else:
+                month = date[2:4]
 
-
+        #   in case we got invalid date
+        if int(month) > 12 or int(day) > 31:
+            return ERROR_MSG
+            
+        return f"{day}/{month}"
+    except:
+        return ERROR_MSG
+    
 from PIL import Image
 
 import os
